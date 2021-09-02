170a18600b2a62efabea0b6c429b19f5967b7834
Koshmanov Ilya <k0shmanov@yandex.ru>
2021-09-02 17:18:19 +0300

    Изменения БД
    Изменены имена БД тк были указаны не корректные
    изменен тестовый класс для работы с базой
    найдена ошибка чтения данных из БД

--- PingerPetProject/PingerCore.cs
+++ PingerPetProject/PingerCore.cs
@@ -9,9 +9,9 @@ using System.Data.SqlClient;
 
 namespace PingerPetProject
 {
-    #region ДОБАВИТЬ ОПИСАНИЕ
-    [Table(Name = "Host")]
-    internal class Host
+    #region инициализация таблиц
+    [Table(Name = "Hosts")]
+    internal class Hosts
     {
         [Column(IsPrimaryKey = true, IsDbGenerated = true)]
         public int hostID { get; set; }
@@ -20,20 +20,32 @@ namespace PingerPetProject
         [Column]
         public string physLocationHost { get; set; }
     }
+    [Table(Name = "CheckingHosts")]
+    internal class CheckingHosts
+    {
+        [Column(IsPrimaryKey = true, IsDbGenerated = true)]
+        public int iteration_num { get; set; }
+        [Column]
+        public int hostID { get; set; }
+        [Column(Name = "hostID")]
+        public bool hostStatus { get; set; }
+    }
     #endregion
     static class PingerCore
     {
         static private string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\AppData\Host.mdf;Integrated Security=True";
-
+       
         public static void test()
         {
             DataContext db = new DataContext(connectionString);
-            // Получаем таблицу пользователей
-            Table<Host> Host = db.GetTable<Host>();
-            foreach (var hosts in Host)
-            {
-                Console.WriteLine("{0} \t{1} \t{2}", hosts.hostID, hosts.hostName, hosts.physLocationHost);
-            }
+            Table<Hosts> Hosts = db.GetTable<Hosts>();
+            Table<CheckingHosts> CheckingHosts = db.GetTable<CheckingHosts>();
+            foreach (var host in Hosts)
+            {Console.WriteLine("{0} \t{1} \t{2}", host.hostID, host.hostName, host.physLocationHost); }
+            Console.WriteLine();
+            foreach (var checkinghost in CheckingHosts)
+            //checkinghost.hostStatus - возвращает все запросы true хотя в бд не так, почему?
+            { Console.WriteLine("{0} \t{1} \t{2}", checkinghost.iteration_num, checkinghost.hostID, checkinghost.hostStatus); }
         }
     }
 }
--- PingerPetProject/Program.cs
+++ PingerPetProject/Program.cs
@@ -15,74 +15,7 @@ namespace PingerPetProject
         static void Main()
         {
             PingerCore.test();
-            Console.ReadLine();
-            //PingerOutputConsole.Begin(20);
+            Console.ReadLine();            
         }
     }
-   /*/убрать класс, тк все данные будут хранится в БД, перенести только обработку Качества
-    class Host
-    {
-        public string hostName { get; set; }
-        public byte pingIterator { get; set; }
-        public bool hostStatus { get; set; }
-        public int qualityLinkHost { get; set; }
-        public string physLocationHost { get; set; }
-        List<bool> QualityArray = new List<bool>();
-        int maxSizeArrayQuality = 0;
-        bool triggerFullArray = false;
-
-
-        public Host(string name, string physLocation)
-        {
-            hostName = name;
-            pingIterator = 0;
-            hostStatus = true;
-            physLocationHost = physLocation;
-            qualityLinkHost = 0;
-        }
-        public int Quality(bool hostStatus)
-        {
-            double LinkTrue = 0;
-            double LinkTrueTemp = 0;
-
-            if (triggerFullArray == true)
-            {
-                if (maxSizeArrayQuality < 1000)
-                {
-                    QualityArray[maxSizeArrayQuality] = hostStatus;
-                }
-                if (maxSizeArrayQuality == 1000)
-                {
-                    QualityArray[maxSizeArrayQuality] = hostStatus;
-                    maxSizeArrayQuality = -1;
-                }
-            }
-            if (triggerFullArray == false)
-            {
-                if (QualityArray.Count < 1000)
-                {
-                    QualityArray.Add(hostStatus);
-                }
-                if (QualityArray.Count == 1000)
-                {
-                    triggerFullArray = true;
-                    maxSizeArrayQuality = -1;
-                    QualityArray.Add(hostStatus);
-                }
-            }
-            for (int i = 0; i < QualityArray.Count; i++)
-            {
-                if (QualityArray[i] == true)
-                {
-                    LinkTrue++;
-                }
-            }
-            double QualityArrayDouble = QualityArray.Count;
-            LinkTrueTemp = LinkTrue / QualityArrayDouble * 100;
-            maxSizeArrayQuality++;
-            qualityLinkHost = (int)LinkTrueTemp;
-            return qualityLinkHost;
-        }
-    }    
-   */
 }
